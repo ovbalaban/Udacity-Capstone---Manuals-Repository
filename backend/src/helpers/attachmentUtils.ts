6b298d89bb1ae90016a62fb5e907dae97cabd728
ovbalaban <38633081+ovbalaban@users.noreply.github.com>
2022-09-03 22:27:24 +0100

    Update attachmentUtils.ts

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -9,14 +9,14 @@ export class AttachmentUtils {
     constructor(
       private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
       private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
-      private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
+//      private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
     ) {}
   
     generatePresignedUrl(manualId: string): string {
       return this.s3.getSignedUrl('putObject', {
         Bucket: this.bucketName,
         Key: manualId,
-        Expires: 3000
+        Expires: 300
       })
     }
   }
